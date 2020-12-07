a008b70928b00c5a7ac58afc6eea83c9a3605e4d
nathanmailhiot <nathanjm12@outlook.com>
2020-12-07 14:49:32 -0500

    Adding pages based on switch

--- admin/posts.php
+++ admin/posts.php
@@ -11,51 +11,24 @@
                             <small>Author</small>
                 </h1>
 
-                <table class="table table-bordered table-hover">
-                    <thead>
-                        <tr>
-                        <th>Id</th>
-                        <th>Author</th>
-                        <th>Title</th>
-                        <th>Category</th>
-                        <th>Status</th>
-                        <th>Image</th>
-                        <th>Tags</th>
-                        <th>Comments</th>
-                        <th>Date</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php 
-                            $query = 'SELECT * FROM posts';
-                            $select_posts = mysqli_query($connection, $query);
+                <?php 
+                
+                if(isset($_GET['source'])) {
+                    $source = $_GET['source'];
+                } else {
+                    $source = '';
+                }
 
-                            while($row = mysqli_fetch_assoc($select_posts)) {
-                                $post_id = $row['id'];
-                                $post_title = $row['post_title'];
-                                $post_author = $row['post_author'];
-                                $post_category_id = $row['post_category_id'];
-                                $post_date = $row['post_date'];
-                                $post_image = $row['post_image'];
-                                $post_tag = $row['post_tag'];
-                                $post_comment_count = $row['post_comment_count'];
-                                $post_status = $row['post_status'];
-
-                                echo "<tr>";
-                                echo "<td>{$post_id}</td>";
-                                echo "<td>{$post_author}</td>";
-                                echo "<td>{$post_title}</td>";
-                                echo "<td>{$post_category_id}</td>";
-                                echo "<td>{$post_status}</td>";
-                                echo "<td><img src='../images/$post_image' width='100' /></td>";
-                                echo "<td>{$post_tag}</td>";
-                                echo "<td>{$post_comment_count}</td>";
-                                echo "<td>{$post_date}</td>";
-                                echo "</tr>";
-                            }
-                        ?>
-                    </tbody>
-                </table>
+                switch($source) {
+                    case "400":
+                        echo '400';
+                    break;
+                    default:
+                        include "includes/view_all_posts.php";
+                    break;
+                }
+                
+                ?>
                 </div>
                 <!-- /.row -->
             </div>
