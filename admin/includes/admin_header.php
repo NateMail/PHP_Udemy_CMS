9f435b59de69c128ee167210a03f0d3403a7b958
nathanmailhiot <nathanjm12@outlook.com>
2020-12-18 15:18:26 -0500

    Checking if admin

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -3,6 +3,16 @@
 <?php include '../includes/db.php' ?>
 <?php include 'functions.php' ?>
 
+<?php 
+
+    if(isset($_SESSION['role'])) {
+        if($_SESSION['role'] === 'subscriber') {
+            header("Location: ../index.php");
+        }
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
