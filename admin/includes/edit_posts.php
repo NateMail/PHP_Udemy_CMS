2f35988b756b78ab3cfd81ef8150d84c4b40ab22
nathanmailhiot <nathanjm12@outlook.com>
2021-01-05 11:31:56 -0500

    Succes message for edit post

--- admin/includes/edit_posts.php
+++ admin/includes/edit_posts.php
@@ -54,6 +54,8 @@
 
         confirm($update_post);
 
+        echo "<p class='bg-success'>Post Updated: <a href='../post.php?p_id={$the_post_id}'>View Post</a> or <a href='posts.php'>Edit More Posts</a></p>";
+
     }
     
 ?>
