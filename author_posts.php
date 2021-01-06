bca7832d90583ce48eab53adc3027cf44ba9a927
nathanmailhiot <nathanjm12@outlook.com>
2021-01-06 13:04:18 -0500

    Authors name leads to authors posts

--- author_posts.php
+++ author_posts.php
@@ -16,9 +16,10 @@
 
             if(isset($_GET['p_id'])) {
                 $post_id = $_GET['p_id'];
+                $the_post_author = $_GET['author'];
             }
             
-            $query = "SELECT * FROM posts WHERE id = $post_id ";
+            $query = "SELECT * FROM posts WHERE post_author = '{$the_post_author}' ";
             $select_all_posts_query = mysqli_query($connection, $query);
 
             while($row = mysqli_fetch_assoc($select_all_posts_query)) {
@@ -38,7 +39,7 @@
                     <a href="#"><?php echo $post_title; ?></a>
                 </h2>
                 <p class="lead">
-                by <a href="index.php"><?php echo $post_author; ?></a>
+                All posts by <?php echo $post_author; ?>
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date?></p>
                 <hr>
@@ -85,68 +86,7 @@
                     
                 }
             ?>
-                <!-- Comments Form -->
-                <div class="well">
-                    <h4>Leave a Comment:</h4>
-                    <form action="" method="post" role="form">
-
-                    <div class="form-group">
-                    <label for="Author">Author</label>
-                            <input type="text" name="comment_author" class="form-control" >
-                        </div>
-                        <div class="form-group">
-                        <label for="Email">Email</label>
-                            <input type="email" class="form-control" name='comment_email'>
-                        </div>
-
-
-                        <div class="form-group">
-                            <label for="Comment">Comment</label>
-                            <textarea class="form-control" name="comment_content" rows="3"></textarea>
-                        </div>
-                        <button type="submit" class="btn btn-primary" name="create_comment">Submit</button>
-                    </form>
-                </div>
-
-                <hr>
-
-                <!-- Posted Comments -->
-
-                <?php 
-                
-            $query = "SELECT * FROM comments WHERE comment_post_id = ${post_id} AND comment_status = 'approved' ORDER BY comment_id DESC ";
-            $select_comment_query = mysqli_query($connection, $query);
-            if(!$select_comment_query){
-                die('Query failed ' . mysqli_error($connection));
-            }
-
-            while($row = mysqli_fetch_assoc($select_comment_query)) {
-                $comment_date = $row['comment_date'];
-                $comment_content = $row['comment_content'];
-                $comment_author = $row['comment_author'];
-
-
-                ?>
-
-                 <!-- Comment -->
-                 <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/64x64" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading"><?php echo $comment_author; ?>
-                            <small><?php echo $comment_date; ?></small>
-                        </h4>
-                        <?php echo $comment_content; ?>
-                    </div>
-                </div>
-
-
-           <?php } ?>
-
-               
             </div>
-
             <!-- Blog Sidebar Widgets Column -->
             <?php include "includes/sidebar.php"?>
 
